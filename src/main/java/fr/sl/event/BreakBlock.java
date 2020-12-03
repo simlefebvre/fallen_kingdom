8210fd74d166099264496cacd8cd108de061504e
Simon <simon.lefebvre@utbm.fr>
2020-12-03 22:26:44 +0100

    positionnement premierre base via fichier de config

--- src/main/java/fr/sl/event/BreakBlock.java
+++ src/main/java/fr/sl/event/BreakBlock.java
@@ -29,28 +29,39 @@ public class BreakBlock implements Listener {
     	Player p = e.getPlayer();
     	Block b = e.getBlock();
         if (p.getGameMode().equals(GameMode.CREATIVE) && e.getPlayer().getInventory().getItemInMainHand().equals(new ItemStack(Material.COMMAND_BLOCK))) {
-        	if(coo1[0] == 0 && coo1[1]==0 && coo1[2]==0) {
+        	if((coo1[0] == 0 && coo1[1]==0 && coo1[2]==0) ||(coo2[0] != 0 && coo2[1] != 0 && coo2[2]!=0)) {
         		coo1[0] = b.getX();
         		coo1[1] = b.getY();
         		coo1[2] = b.getZ();
+        		coo2[0] = 0;
+        		coo2[1] = 0;
+        		coo2[2] = 0;
         		wd = b.getWorld();
-        		p.sendMessage(String.format("Position 1 établie temporairement en %i,%i,%i", coo1[0],coo1[1],coo1[2]));
+        		p.sendMessage(String.format("Position 1 établie temporairement en %d,%d,%d", coo1[0],coo1[1],coo1[2]));
         	}else {
         		coo2[0] = b.getX();
         		coo2[1] = b.getY();
         		coo2[2] = b.getZ();
-        		p.sendMessage(String.format("Position 2 établie temporairement en %i,%i,%i", coo2[0],coo2[1],coo2[2]));
+        		p.sendMessage(String.format("Position 2 établie temporairement en %d,%d,%d", coo2[0],coo2[1],coo2[2]));
         	}
         	e.setCancelled(true);
         }else if(p.getGameMode().equals(GameMode.CREATIVE) && e.getPlayer().getInventory().getItemInMainHand().equals(new ItemStack(Material.WOODEN_AXE))) {
-        	mc.getConfig().set("base1.x", coo1[0]);
-        	mc.getConfig().set("base1.y", coo1[1]);
-        	mc.getConfig().set("base1.z", coo1[2]);
-        	mc.getConfig().set("base1.world", wd);
-        	mc.getConfig().set("base2.x", coo2[0]);
-        	mc.getConfig().set("base2.y", coo2[1]);
-        	mc.getConfig().set("base2.z", coo2[2]);
-        	mc.getConfig().set("base2.world", wd);
+        	mc.getConfig().set("base1.pos1.x", coo1[0]);
+        	mc.getConfig().set("base1.pos1.y", coo1[1]);
+        	mc.getConfig().set("base1.pos1.z", coo1[2]);
+        	mc.getConfig().set("base1.world", wd.getName());
+        	mc.getConfig().set("base1.pos2.x", coo2[0]);
+        	mc.getConfig().set("base1.pos2.y", coo2[1]);
+        	mc.getConfig().set("base1.pos2.z", coo2[2]);
+        	mc.saveConfig();
+        	p.sendMessage("Le fichier de config à bien était mis à jours");
+        	
+        	coo1[0] = 0;
+    		coo1[1] = 0;
+    		coo1[2] = 0;
+    		coo2[0] = 0;
+    		coo2[1] = 0;
+    		coo2[2] = 0;
         }
     }
 }
