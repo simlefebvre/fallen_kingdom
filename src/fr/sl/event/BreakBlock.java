ba054808e5bd7f4ee0cbece3fb7338831ca2a987
Simon <simon.lefebvre@utbm.fr>
2020-12-03 19:48:42 +0100

    test

---
+++ src/fr/sl/event/BreakBlock.java
@@ -0,0 +1,10 @@
+package fr.sl.event;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+public class BreakBlock implements Listener {
+	
+	@EventHandler
+	
+}
--- src/fr/sl/main/MainClass.java
+++ src/fr/sl/main/MainClass.java
@@ -3,6 +3,7 @@ package fr.sl.main;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import fr.sl.commande.CommandeManager;
+import fr.sl.event.BreakBlock;
 
 public class MainClass extends JavaPlugin {
 
@@ -10,7 +11,9 @@ public class MainClass extends JavaPlugin {
 	
 	public void onEnable() {
 		System.out.println("[FALLEN KINGDOM] le plugin est bien lancé");
+		this.saveDefaultConfig();
 		this.getCommand("fk").setExecutor(new CommandeManager());
+		getServer().getPluginManager().registerEvents(new BreakBlock(), this);
 	}
 	
 	public void onDisable() {
